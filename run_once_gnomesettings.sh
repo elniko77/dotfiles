6bb70af9fc99bcaa46be83e94e6d347a4271a03d
Nicolas Sosa (nss) <nico77@gmail.com>
2024-01-08 14:57:40 -0300

    max min buttons

--- run_once_gnomesettings.sh
+++ run_once_gnomesettings.sh
@@ -2,3 +2,5 @@
 
 # Fix hardcoded user
 gsettings set org.gnome.desktop.background picture-uri file:///home/nss/Pictures/wallpaper.png
+# Add close, maxim, mini to windows (to the right)
+gsettings set org.gnome.desktop.wm.preferences button-layout ':close,minimize,maximize'

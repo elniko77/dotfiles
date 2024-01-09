e0ad1feb4b0f522077b6b632420f1731ca9e8a60
Nicolas Sosa (nss) <nico77@gmail.com>
2024-01-09 07:09:34 -0300

    Dark mode, packages

--- run_once_gnomesettings.sh
+++ run_once_gnomesettings.sh
@@ -4,3 +4,6 @@
 gsettings set org.gnome.desktop.background picture-uri file:///home/nss/Pictures/wallpaper.png
 # Add close, maxim, mini to windows (to the right)
 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
+# Set dark mode
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+

dcf67454b87f434f1bc74fd4795099b016506a55
Nicolas Sosa (nss) <nico77@gmail.com>
2024-01-09 07:29:51 -0300

    Dark wallpaper

--- run_once_gnomesettings.sh
+++ run_once_gnomesettings.sh
@@ -2,6 +2,9 @@
 
 # Fix hardcoded user
 gsettings set org.gnome.desktop.background picture-uri file:///home/nss/Pictures/wallpaper.png
+gsettings set org.gnome.desktop.background picture-uri-dark file:///home/nss/Pictures/wallpaper.png
+
+
 # Add close, maxim, mini to windows (to the right)
 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
 # Set dark mode

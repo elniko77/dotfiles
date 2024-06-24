89f6542682582f5b685a01200b404f4762a6b5dd
Nicolas Sosa (nss) <nico77@gmail.com>
2024-06-24 16:37:42 +0000

    gui config commented

--- run_once_gnomesettings.sh
+++ run_once_gnomesettings.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Fix hardcoded user
-gsettings set org.gnome.desktop.background picture-uri file:///home/nss/Pictures/wallpaper.png
-gsettings set org.gnome.desktop.background picture-uri-dark file:///home/nss/Pictures/wallpaper.png
+#gsettings set org.gnome.desktop.background picture-uri file:///home/nss/Pictures/wallpaper.png
+3gsettings set org.gnome.desktop.background picture-uri-dark file:///home/nss/Pictures/wallpaper.png
 
 
 # Add close, maxim, mini to windows (to the right)
-gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
+#gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
 # Set dark mode
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+#gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 

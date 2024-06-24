8585cb130a9835e23a899b3de45fc5a78e4a91bb
Nicolas Sosa (nss) <nico77@gmail.com>
2024-06-24 16:40:51 +0000

    error gsettings

--- run_once_gnomesettings.sh
+++ run_once_gnomesettings.sh
@@ -2,7 +2,7 @@
 
 # Fix hardcoded user
 #gsettings set org.gnome.desktop.background picture-uri file:///home/nss/Pictures/wallpaper.png
-3gsettings set org.gnome.desktop.background picture-uri-dark file:///home/nss/Pictures/wallpaper.png
+#gsettings set org.gnome.desktop.background picture-uri-dark file:///home/nss/Pictures/wallpaper.png
 
 
 # Add close, maxim, mini to windows (to the right)

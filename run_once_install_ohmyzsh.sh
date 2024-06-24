741df4b9d50e40fdb551f0247ff8dc6e739276c8
Nicolas Sosa (nss) <nico77@gmail.com>
2024-06-24 16:45:26 +0000

    check if zsh installed

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -f /usr/bin/zsh ]; then
+    sudo apt install zsh
+fi
+
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh

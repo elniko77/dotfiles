da551a4ebcbbd86bbc85006179acd1c6130f695a
nss <nico77@gmail.com>
2026-03-12 11:53:36 -0300

    Addopt files
    Added stow commands for adopting multiple configurations.

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+stow --adopt config
+stow --adopt git
+stow --adopt scripts
+stow --adopt shell
+stow --adopt tmux
 
-stow --adopt *
 git restore . 
+
 stow config
 stow git
 stow scripts
